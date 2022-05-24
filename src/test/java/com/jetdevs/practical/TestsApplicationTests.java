ccd63b28fcf49f762deec92069abfdeebd235850
Kunal <kunal.barchha1285@gmail.com>
2022-05-24 14:53:36 +0530

    "initial commit"

---
+++ src/test/java/com/jetdevs/practical/TestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jetdevs.practical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
